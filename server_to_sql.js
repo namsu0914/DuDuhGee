317c400b077d7fd23ba2051cba3d6623c7523cc2
eu721gine <0721juli@naver.com>
2023-02-20 16:17:28 +0900

    eugine commit

---
+++ server_to_sql.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql');  // mysql 모듈 로드
+const conn = {  // mysql 접속 설정
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: '유진pw',
+    database: 'Users'
+};
+
+let connection = mysql.createConnection(conn); // DB 커넥션 생성
+connection.connect();   // DB 접속
+ 
+let sql = "INSERT INTO `users` (`num_sign`,`id`,`password`,`name`) VALUES (2,'test2','hello','lee');";
+ 
+connection.query(sql, function (err, results, fields) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(results);
+});
+ 
+sql = "SELECT * FROM users";
+ 
+connection.query(sql, function (err, results, fields) { 
+    if (err) {
+        console.log(err);
+    }
+    console.log(results);
+});
+ 
+ 
+connection.end(); 
