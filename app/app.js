302ebb3337554620e49bdde959ff8f7af81cd7cc
southbreath <tnals8583@naver.com>
2023-03-10 13:38:53 +0900

    save

--- app/app.js
+++ app/app.js
@@ -7,6 +7,9 @@ const session = require('express-session');
 const crypto = require('crypto');
 const FileStore = require('session-file-store')(session); // 세션을 파일에 저장
 const cookieParser = require('cookie-parser');
+const post = require('./router/routs')
+app.use('/post_pa')
+
 
 // express 설정 1
 const app = express();
@@ -26,6 +29,7 @@ app.set('views', __dirname + '\\views');
 app.set('view engine','ejs');
 
 // 정제 (미들웨어) 5
+app.use(bodyParser.json()); //json으로 파싱
 app.use(bodyParser.urlencoded({extended:false}));
 
 // 세션 (미들웨어) 6
@@ -38,8 +42,8 @@ app.use(session({
 
 // 메인페이지
 app.get('/',(req,res)=>{
-    console.log('메인페이지 작동');
-    console.log(req.session);
+    //console.log('메인페이지 작동');
+    //console.log(req.session);
     if(req.session.is_logined == true){
         res.render('index',{
             is_logined : req.session.is_logined,
@@ -52,56 +56,26 @@ app.get('/',(req,res)=>{
     }
 });
 
-app.get('/register',(req,res)=>{
-    console.log('회원가입 페이지');
-    res.render('register');
-});
-
-app.post('/register',(req,res)=>{
-    console.log('회원가입 하는중')
-    const body = req.body;
-    const id = body.id;
-    const pw = body.pw;
-    //const 
-    //const name = body.name;
-    //const age = body.age;
-
-    client.query('select * from users where id=?',[id],(err,data)=>{
-        if(data.length == 0){
-            i=0;
-            console.log('회원가입 성공');
-            client.query('insert into users(id, pw, num) values(?,?,?)',[
-                id, pw , i
-            ]);
-            i++;
-            res.redirect('/');
-        }else{
-            console.log('회원가입 실패');
-            res.send('<script>alert("회원가입 실패");</script>')
-            res.redirect('/login');
-        }
-    });
-});
 
 // 로그인
-app.get('/login',(req,res)=>{
+app.get('/login.ejs',(req,res)=>{
     console.log('로그인 작동');
-    res.render('login');
+    res.render('login.ejs');
 });
 
-app.post('/login',(req,res)=>{
+app.post('/login.ejs',(req,res)=>{
     const body = req.body;
     const id = body.id;
     const pw = body.pw;
-
-    client.query('select * from userdata where id=?',[id],(err,data)=>{
+    console.log(id);
+        client.query('select * from duduhgeedb.users where id=?',[id],(err,data)=>{
         // 로그인 확인
-        console.log(data[0]);
-        console.log(id);
-        console.log(data[0].id);
-        console.log(data[0].pw);
-        console.log(id == data[0].id);
-        console.log(pw == data[0].pw);
+        //console.log(data[0]);
+        //console.log(id);
+        //console.log(data[0].id);
+        //console.log(data[0].pw);
+        //console.log(id == data[0].id);
+        //console.log(pw == data[0].pw);
         if(id == data[0].id || pw == data[0].pw){
             console.log('로그인 성공');
             // 세션에 추가
@@ -113,16 +87,45 @@ app.post('/login',(req,res)=>{
                 res.render('index',{ // 정보전달
                     name : data[0].name,
                     id : data[0].id,
-                    age : data[0].age,
                     is_logined : true
                 });
             });
         }else{
             console.log('로그인 실패');
-            res.render('login');
+            res.render('login.ejs');
         }
     });
     
+});
+
+app.get('/register',(req,res)=>{
+    console.log('회원가입 페이지');
+    res.render('register');
+});
+
+app.post('/register',(req,res)=>{
+    console.log('회원가입 하는중')
+    const body = req.body;
+    const id = body.id;
+    const pw = body.pw;
+    const name = body.name;
+    //const age = body.age;
+
+    client.query('select * from duduhgeedb.users where id=?',[id],(err,data)=>{
+        if(data.length == 0){
+            i=0;
+            console.log('회원가입 성공');
+            client.query('insert into users(id, pw, name) values(?,?,?)',[
+                id, pw , i
+            ]);
+            i++;
+            res.redirect('/');
+        }else{
+            console.log('회원가입 실패');
+            res.send('<script>alert("회원가입 실패");</script>')
+            res.redirect('/login.ejs');
+        }
+    });
 });
 
 // 로그아웃
